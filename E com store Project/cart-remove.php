e6627f354507b1431e32a2cb198014891aff93ea
Musharraf Hussain <musharraf1487@gmail.com>
2024-07-10 00:41:40 +0500

    Add files via upload

---
+++ E com store Project/cart-remove.php
@@ -0,0 +1,12 @@
+<?php
+
+require("includes/common.php");
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $item_id = $_GET["id"]; 
+    $user_id = $_SESSION['user_id'];
+    //delete karega ids ko match kar k
+    $query = "DELETE FROM user_item WHERE item_id='$item_id' AND user_id='$user_id' AND status= 1";
+    $res = mysqli_query($con, $query) or die($mysqli_error($con));
+    header("location:cart.php");
+}
+?>
